9732019cec4e2ed0211ec4149b51165f6328222d
kemasfaris <110803730+kemasfaris@users.noreply.github.com>
2022-09-08 09:46:11 +0700

    Add files via upload
    Menambah Header

---
+++ Headerfaris/index.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device.width, initial-scale=1.0">
+    <title>Portal pekerjaan untuk alumni</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="header-logo">
+            Selamat datang
+        </div class="header-nav">
+        <div>
+            <ul>
+                <li>Tentang kami</li>
+                <li>info</li>
+                <li>hubungi</li>
+            </ul>
+        </div>
+    </header>
+
+</body>
+</html>
+
+
